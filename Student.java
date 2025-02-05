ba9c1ec5c393d02b3f9ed43f9463ff08eda2fc1f
tagizadea <tagizadea01@gmail.com>
2025-02-05 13:43:48 +0400

    init

---
+++ Student.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Student{
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("Student ID: ");
+        String s = sc.nextLine();
+        
+    }
+}
